a7cc6758af1f75776e68960e714e11fdbfef09a7
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2023-12-19 23:08:13 -0500

    Prettify display of selectable fields
    In turn, we've also had to convert the prettified string back to the
    keyword-symbol-based canonical representation.

--- password-manager.el
+++ password-manager.el
@@ -45,6 +45,14 @@ element (as returned by `org-element-parse-buffer'.)"
                                     (list property (plist-get properties property)))
                                   user-keys)))))
 
+(defun pm--pretty-print-user-key (user-key)
+  "Create an aesthetic version of USER-KEY."
+  (replace-regexp-in-string "-" " " (capitalize (pm--derive-raw-name user-key))))
+
+(defun pm--canonicalize-pretty-key (pretty-key)
+  "Reverse pretty printing of USER-KEY."
+  (intern (concat ":" (replace-regexp-in-string " " "-" (upcase pretty-key)))))
+
 (defun pm-compile-data ()
   "Compile an alist mapping headlines (which should be the names of
 the various services you use) to username and password data.
@@ -61,15 +69,20 @@ clipboard."
       (interactive)
       (let* ((name (completing-read "Service: " (mapcar #'car user-data) nil t))
              (current-properties (cdr (assoc name user-data))))
-        (let ((field (completing-read "Copy what: " (pm--filter-user-keys current-properties) nil t)))
-          (gui-set-selection 'CLIPBOARD (plist-get current-properties (intern field))))))))
+        (let ((field (pm--canonicalize-pretty-key
+                      (completing-read "Copy what: "
+                                       (mapcar #'pm--pretty-print-user-key
+                                               (pm--filter-user-keys current-properties))
+                                       nil t))))
+          (gui-set-selection 'CLIPBOARD (plist-get current-properties field)))))))
 
 (defun pm-lookup ()
   "The driver for looking up usernames and passwords."
   (interactive)
   (let ((result (call-interactively (pm-compile-data))))
-    (or result
-        (user-error "Drawer property missing"))))
+    (if result
+        (message "Copied data to clipboard")
+      (user-error "Drawer property missing"))))
 
 (provide 'password-manager)
 
