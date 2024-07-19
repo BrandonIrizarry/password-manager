a06f2eae83a0ea6d8a400b7ba53442f8f73fec3f
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-07-18 22:38:50 -0400

    Write 'password-manager-copy-property-to-clipboard'
    This function uses the helpers 'password-manager--get-property' and
    'password-manager--get-all-headline-titles'.

--- password-manager.el
+++ password-manager.el
@@ -1,5 +1,42 @@
 ;; -*- lexical-binding: t; -*-
 
+(require 'org-element)
+
+
+(defun pm--get-property (headline property)
+  "Get PROPERTY from property drawer beneath HEADLINE."
+  (save-excursion
+    (goto-char 1)
+    (re-search-forward (rx-to-string `(seq "*" (+ space) ,headline)))
+    (beginning-of-line)
+    (let ((drawer-properties (cadr (org-element-headline-parser))))
+      (plist-get drawer-properties property))))
+
+
+(defun pm--get-all-headline-titles ()
+  "Return a list of all headline titles in this Org buffer."
+  (let ((tree (org-element-parse-buffer 'headline)))
+    (org-element-map tree 'headline
+      (lambda (headline)
+	(org-element-property :title headline)))))
+
+(defun pm-copy-property-to-keyboard (service property)
+  "Copy PROPERTY, filed in SERVICE's property drawer, to the system clipboard.
+
+When called interactively, SERVICE and PROPERTY are prompted for
+from the user."
+  (interactive
+   (let ((properties-readable-to-internal '(("username" . :USERNAME)
+					    ("password" . :PASSWORD))))
+     (list
+      (completing-read "Service: " (pm--get-all-headline-titles))
+      (let ((readable (completing-read "Which property: " properties-readable-to-internal)))
+	(cdr (assoc readable properties-readable-to-internal))))))
+  (if (gui-set-selection 'CLIPBOARD (pm--get-property service property))
+      (message "Copied data to clipboard")
+    (user-error "Drawer property missing")))
+
+
 (provide 'password-manager)
 
 ;; Local Variables:
